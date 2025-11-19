f71075a69fba7864d3c493c40a0bf9675a58c020
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-19 19:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
     //Write your code here
-
+    const a=document.querySelector("a");
+	const strong=document.querySelectorAll("strong");
+		strong.forEach((e)=>{
+			e.style.color="green";
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+const a=document.querySelector("a");
+	const strong=document.querySelectorAll("strong");
+		strong.forEach((e)=>{
+			e.style.color="";
+		})
     
 }
